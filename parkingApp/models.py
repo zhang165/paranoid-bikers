7f13884e153256755c861ace47443985d7085da0
Jolene Wong <jwong91@gmail.com>
2015-06-15 13:32:53 -0700

    commiting readme and updating models

--- parkingApp/models.py
+++ parkingApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+	lat = models.FloatField()
+	lon = models.FloatField()
+
+	def __unicode__(self):
+		return self.Location
+
+class ParkingSpot(models.Model):
+	location = models.OneToOneField(Location)
+	name = models.CharField(max_length=120)
+	toll = models.DecimalField(max_digits= 4, decimal_places=2)
+	#picture
+
+	def __unicode__(self):
+		return self.ParkingSpot
