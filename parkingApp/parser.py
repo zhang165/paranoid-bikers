e82809b8b6fc8c77060891e004b820af25d111b1
Jolene Wong <jwong91@gmail.com>
2015-06-17 10:39:42 -0700

    updating the parser to have less hardcoded information

--- parkingApp/parser.py
+++ parkingApp/parser.py
@@ -3,6 +3,12 @@ import urllib.request
 from zipfile import ZipFile
 
 def createXML(kml, filename):
+	'''
+	String, String -> None
+
+	takes a kml filename and xml filename and parses
+	through the kml to write into an xml 
+	'''
 	root = ET.Element("Document")
 	comment = ET.Comment("Prepared for ParanoidBikers")
 	root.append(comment)
@@ -19,6 +25,7 @@ def createXML(kml, filename):
 		description = ET.SubElement(placemark, "description")
 		description.text = kml[i][1].text
 
+
 		# add a Point to Placemark
 		point = ET.SubElement(placemark, "Point")
 
@@ -32,10 +39,15 @@ def createXML(kml, filename):
 		tree.write(fh)
 
 def toKML(url):
+	'''
+	string -> None
+
+	takes url of kmz file and unzips it and extracts the kml file
+	'''
 	kmz_path = url
 	urllib.request.urlretrieve(kmz_path, 'kmz_file.kmz')
 	kmz_file = ZipFile('kmz_file.kmz')
-	kml_file = kmlz_file.extract('motorcyle_parking.kml')
+	kml_file = kmz_file.extract('motorcycle_parking.kml')
 	kmz_file.close()
 
 def getRoot(kmlFilename):
@@ -45,28 +57,30 @@ def getRoot(kmlFilename):
 	return folder
 
 
-def parser(url, kmlFilename, filename):
+def parser(url, kmlFilename, xmlfilename):
 	toKML(url)
 	kmlFilename = 'motorcycle_parking.kml'
 	kml = getRoot(kmlFilename)
-	createXML(kml, filename)
+	createXML(kml, xmlfilename)
 
 if __name__ == '__main__':
 	# The path to our kmz file
 	KMZ_PATH = 'http://data.vancouver.ca/download/kml/motorcycle_parking.kmz'
 	# Retrieve the file and save it locally
-	urllib.request.urlretrieve(KMZ_PATH, 'motorcycle_parking.kmz')
+	#urllib.request.urlretrieve(KMZ_PATH, 'motorcycle_parking.kmz')
 	# Unzip the kmz file
-	KMZ_FILE = ZipFile('motorcycle_parking.kmz', 'r')
+	#KMZ_FILE = ZipFile('motorcycle_parking.kmz', 'r')
 	# Open the kml file or extract the kml file from the kmz file
-	KML_FILE = KMZ_FILE.extract('motorcycle_parking.kml')
+	#KML_FILE = KMZ_FILE.extract('motorcycle_parking.kml')
 	# Close the kmz file
-	KMZ_FILE.close()
+	#KMZ_FILE.close()
 
-	tree = ET.parse(KML_FILE)
-	root = tree.getroot()
-	folder = root[0][3]
+	#tree = ET.parse(KML_FILE)
+	#root = tree.getroot()
+	#folder = root[0][3]
 
-	filename = "motorcycle_parking.xml"
-	createXML(folder, filename)
+	xmlfilename = "motorcycle_parking.xml"
+	kmlfilename = 'motorcycle_parking.kml'
+	# createXML(folder, xmlfilename)
+	parser(KMZ_PATH, kmlfilename, xmlfilename)
 	
