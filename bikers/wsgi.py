4208b175af1b2d09e7e932b420d1865a4f75c1a4
Jolene Wong <jwong91@gmail.com>
2015-06-18 17:49:38 -0700

    updated wsgi

--- bikers/wsgi.py
+++ bikers/wsgi.py
@@ -7,10 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bikers.settings")
-
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
