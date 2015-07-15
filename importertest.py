6e8203999f27be8d5ccca62dddeec0cb0a94ef69
Jolene Wong <jwong91@gmail.com>
2015-07-15 15:04:42 -0700

    updating tests

--- importertest.py
+++ importertest.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 
 if __name__ == "__main__":
 	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bikers.settings')
-	from parkingApp.models import Crime
+	from parkingApp.models import Crime, LowCrimePlacemark
 	#import parser, importer
 
 class SimpleTestCase(unittest.TestCase):
@@ -13,7 +13,13 @@ class SimpleTestCase(unittest.TestCase):
 		print("Running tests")
 		
 	def testPopulate(self):
+		print("Running importer")
 		markers = Crime.objects.values('description', 'address', 'lat', 'lon')
 		assert len(markers) == 7864
+
+	def testFilter(self):
+		print("Running filter")
+		markers = LowCrimePlacemark.objects.values('name', 'lat', 'lon', 'placemark_id', 'rate', 'credit_card', 'location', 'intersection', 'time', 'link')
+		assert len(markers) == 29
 
 unittest.main()
